f9b2c5b17b32ed6aaf81f58d1613023c575edd6f
nimos <nimos.cmp@gmail.com>
2021-04-15 20:38:09 -0500

    Create section component for Login

---
+++ src/components/login/Section.jsx
@@ -0,0 +1,19 @@
+import { Grid } from '@material-ui/core'
+import React from 'react'
+import useStyles from '../../styles/Styles';
+
+export default function Section() {
+    //Declare constante from styles component
+    const classes = useStyles();
+
+    return (
+        <Grid container item xs={12} sm={6} className={classes.bgLight} justify="center">
+            <section className={classes.sectionContainer}>
+                <div className={classes.formContainer}>
+                    <h2 className={classes.fw400}>¿Listo para <strong className={classes.iconsLight}>conectarte</strong> con tus amigos?</h2>
+                    <p>Esta plataforma te permite unirte con cientos de personas alrededor del mundo</p>
+                </div>
+            </section>
+        </Grid>
+    )
+}
